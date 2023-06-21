2d0e2c070892ca925acd41a44d29569f6321ac20
ThomasMcDonald904 <naynaytom3000@gmail.com>
2023-06-20 21:36:30 -0400

    Bug Fix: can't find intersection
    Cause: ThreeDoorsLFR was missing WallB group

--- Scripts/Room.gd
+++ Scripts/Room.gd
@@ -5,40 +5,42 @@ class_name Room
 var grid_position = Vector2(0, 0)
 
 func _ready():
-    if get_parent().name != "RoomCache":
-        visible = true
+	if get_parent().name != "RoomCache":
+		visible = true
 
 func get_side_type(side: String) -> String:
-    match side:
-        "right":
-            return "Wall" if "WallR" in get_groups() else "Door"
-        "left":
-            return "Wall" if "WallL" in get_groups() else "Door"
-        "back":
-            return "Wall" if "WallB" in get_groups() else "Door"
-        "front":
-            return "Wall" if "WallF" in get_groups() else "Door"
-        _:
-            assert(side in ["right", "left", "front", "back"], "Given side to get type not part of permited sides")
-            return "Error"
+	match side:
+		"right":
+			return "Wall" if "WallR" in get_groups() else "Door"
+		"left":
+			return "Wall" if "WallL" in get_groups() else "Door"
+		"back":
+			return "Wall" if "WallB" in get_groups() else "Door"
+		"front":
+			return "Wall" if "WallF" in get_groups() else "Door"
+		_:
+			assert(side in ["right", "left", "front", "back"], "Given side to get type not part of permited sides")
+			return "Error"
 
 func set_grid_position(grid: Grid, _grid_position: Vector2):
-    grid.set_position_in_grid(_grid_position, self)
-    grid_position = _grid_position
+	grid.set_position_in_grid(_grid_position, self)
+	grid_position = _grid_position
 
 func get_grid_position():
-    return grid_position
+	return grid_position
 
 func get_accessible_neighbors() -> Array:
-    var neighbor_grid_positions = []
-    for door in get_node("DoorPositions").get_children():
-        match int(door.rotation_degrees.y):
-            -90:
-                neighbor_grid_positions.append(get_grid_position() + Vector2(-1, 0))
-            90:
-                neighbor_grid_positions.append(get_grid_position() + Vector2(1, 0))
-            180:
-                neighbor_grid_positions.append(get_grid_position() + Vector2(0, -1))
-            0:
-                neighbor_grid_positions.append(get_grid_position() + Vector2(0, 1))
-    return neighbor_grid_positions
+	var neighbor_grid_positions = []
+	
+	for group in get_groups():
+		match group:
+			"DoorR":
+				neighbor_grid_positions.append(get_grid_position() + Vector2(1, 0))
+			"DoorL":
+				neighbor_grid_positions.append(get_grid_position() + Vector2(-1, 0))
+			"DoorF":
+				neighbor_grid_positions.append(get_grid_position() + Vector2(0, 1))
+			"DoorB":
+				neighbor_grid_positions.append(get_grid_position() + Vector2(0, -1))
+	return neighbor_grid_positions
+
--- Scripts/RoomSpawner.gd
+++ Scripts/RoomSpawner.gd
@@ -5,97 +5,104 @@ const MAX_ROOM_SPAWN = 6
 var grid: Grid = Grid.new(20, 20)
 
 var side_directions = {
-    "front": Vector2(0, 1),
-    "left": Vector2(-1, 0),
-    "back": Vector2(0, -1),
-    "right": Vector2(1, 0)
+	"front": Vector2(0, 1),
+	"left": Vector2(-1, 0),
+	"back": Vector2(0, -1),
+	"right": Vector2(1, 0)
 }
 
 func _ready():
-    var initial_room: Room = $"../RoomCache".get_children().pick_random().duplicate()
-    initial_room.set_grid_position(grid, Vector2(0, 0))
-    
-    for valid_neighbor_position in initial_room.get_accessible_neighbors():
-        var sets = []
-        if grid.get_room_at_position(valid_neighbor_position) == null:
-            for direction in side_directions:
-                var side_type_check_room: Room = grid.get_room_at_position(side_directions[direction] + valid_neighbor_position)
-                if side_type_check_room == null:
-                    sets.append(create_set_for_direction(["Wall", "Door"].pick_random(), direction))
-                else:
-                    sets.append(create_set_for_direction_and_room(direction, side_type_check_room))
-            var possible_rooms = intersect_arrays(sets[0], sets[1], sets[2], sets[3])
-            print("-> ", sets[0], "\n", "-> ", sets[1], "\n", "-> ", sets[2], "\n", "-> ", sets[3], "\n")
-            print(possible_rooms)
-            var chosen_room: Room = possible_rooms.pick_random().duplicate()
-            chosen_room.set_grid_position(grid, valid_neighbor_position)
-    grid.spawn_grid(self)
-    $"../RoomCache".queue_free()
-    
+	var initial_room: Room = $"../RoomCache".get_children().pick_random().duplicate()
+	initial_room.set_grid_position(grid, Vector2(0, 0))
+	print(initial_room.get_accessible_neighbors())
+	for valid_neighbor_position in initial_room.get_accessible_neighbors():
+		var debug_list = []
+		var sets = []
+		for direction in side_directions:
+			var side_type_check_room: Room = grid.get_room_at_position(side_directions[direction] + valid_neighbor_position)
+			if side_type_check_room == null:
+				var type_choice = ["Wall", "Door"].pick_random()
+				debug_list.append([type_choice, direction])
+				sets.append(create_set_for_direction(type_choice, direction))
+			else:
+				debug_list.append([side_type_check_room, direction])
+				sets.append(create_set_for_direction_and_room(direction, side_type_check_room))
+		var possible_rooms = intersect_arrays(sets[0], sets[1], sets[2], sets[3])
+#			print("-> ", sets[0], "\n", "-> ", sets[1], "\n", "-> ", sets[2], "\n", "-> ", sets[3], "\n")
+#			print(possible_rooms)
+		print(valid_neighbor_position, " ", debug_list)
+		var chosen_room: Room = possible_rooms.pick_random()
+		var to_add = chosen_room.duplicate()
+		to_add.set_grid_position(grid, valid_neighbor_position)
+	print("\n", "-----------------", "\n")
+	grid.spawn_grid(self)
+	$"../RoomCache".queue_free()
+	
 
 func _process(_delta):
-    if Input.is_action_just_released("DebugKey"):
-        get_tree().reload_current_scene()
+	if Input.is_action_just_released("DebugKey"):
+		get_tree().reload_current_scene()
 
 
 
 func intersect_arrays(arr1, arr2, arr3, arr4) -> Array:
-    var arr2_dict = {}
-    for v in arr2:
-        arr2_dict[v] = true
+	var arr2_dict = {}
+	for v in arr2:
+		arr2_dict[v] = true
 
-    var arr3_dict = {}
-    for v in arr3:
-        arr3_dict[v] = true
+	var arr3_dict = {}
+	for v in arr3:
+		arr3_dict[v] = true
 
-    var arr4_dict = {}
-    for v in arr4:
-        arr4_dict[v] = true
+	var arr4_dict = {}
+	for v in arr4:
+		arr4_dict[v] = true
 
-    var intersection = []
-    for v in arr1:
-        if arr2_dict.has(v) and arr3_dict.has(v) and arr4_dict.has(v):
-            intersection.append(v)
+	var intersection = []
+	for v in arr1:
+		if arr2_dict.has(v) and arr3_dict.has(v) and arr4_dict.has(v):
+			intersection.append(v)
 
-    return intersection
+	return intersection
 
 func create_set_for_direction(surface_type, direction):
-    match surface_type:
-        "Door":
-            match direction:
-                "front":
-                    return get_tree().get_nodes_in_group("DoorF")
-                "back":
-                    return get_tree().get_nodes_in_group("DoorB")
-                "left":
-                    return get_tree().get_nodes_in_group("DoorL")
-                "right":
-                    return get_tree().get_nodes_in_group("DoorR")
-        "Wall":
-            match direction:
-                "front":
-                    return get_tree().get_nodes_in_group("WallF")
-                "back":
-                    return get_tree().get_nodes_in_group("WallB")
-                "left":
-                    return get_tree().get_nodes_in_group("WallL")
-                "right":
-                    return get_tree().get_nodes_in_group("WallR")
+	match surface_type:
+		"Door":
+			match direction:
+				"front":
+					return get_tree().get_nodes_in_group("DoorF")
+				"back":
+					return get_tree().get_nodes_in_group("DoorB")
+				"left":
+					return get_tree().get_nodes_in_group("DoorL")
+				"right":
+					return get_tree().get_nodes_in_group("DoorR")
+		"Wall":
+			match direction:
+				"front":
+					return get_tree().get_nodes_in_group("WallF")
+				"back":
+					return get_tree().get_nodes_in_group("WallB")
+				"left":
+					return get_tree().get_nodes_in_group("WallL")
+				"right":
+					return get_tree().get_nodes_in_group("WallR")
 
 func create_set_for_direction_and_room(direction: String, room_to_check: Room):
-    match direction:
-        "right":
-            var side_type = room_to_check.get_side_type("left")
-            return create_set_for_direction(side_type, "right")
-        "left":
-            var side_type = room_to_check.get_side_type("right")
-            return create_set_for_direction(side_type, "left")
-        "back":
-            var side_type = room_to_check.get_side_type("front")
-            return create_set_for_direction(side_type, "back")
-        "front":
-            var side_type = room_to_check.get_side_type("back")
-            return create_set_for_direction(side_type, "front")
+#	assert(room_to_check == Room, "trying to get side of not room")
+	match direction:
+		"right":
+			var side_type = room_to_check.get_side_type("left")
+			return create_set_for_direction(side_type, "right")
+		"left":
+			var side_type = room_to_check.get_side_type("right")
+			return create_set_for_direction(side_type, "left")
+		"back":
+			var side_type = room_to_check.get_side_type("front")
+			return create_set_for_direction(side_type, "back")
+		"front":
+			var side_type = room_to_check.get_side_type("back")
+			return create_set_for_direction(side_type, "front")
 
 
 #func spawnRoomToDoor(room, room_door_index: int, mother_room, mother_room_door_index: int):
